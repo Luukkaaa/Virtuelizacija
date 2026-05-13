1c450f8f2e232e91db1e58f80aeeef9c455d13b7
Luka <luka.laketiic@gmail.com>
2026-05-13 21:58:07 +0200

    Definisanje WCF ugovora i modela (Common projekat)

---
+++ Battery/Common/IEisService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IEisService
+    {
+        [OperationContract]
+        ServerStatus StartSession(EisMeta meta);
+
+        [OperationContract]
+        [FaultContract(typeof(ValidationFault))]
+        [FaultContract(typeof(DataFormatFault))]
+        ServerStatus PushSample(EisSample sample);
+
+        [OperationContract]
+        ServerStatus EndSession();
+    }
+}
---
+++ Battery/Common/Models.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum ServerStatus
+    {
+        ACK, NACK, IN_PROGRESS, COMPLETED
+    }
+
+    [DataContract]
+    public class EisMeta
+    {
+        [DataMember] public string BatteryId { get; set; }
+        [DataMember] public string TestId { get; set; }
+        [DataMember] public string SoC { get; set; }
+        [DataMember] public string FileName { get; set; }
+        [DataMember] public int TotalRows { get; set; }
+    }
+
+    [DataContract]
+    public class EisSample
+    {
+        [DataMember] public int RowIndex { get; set; }
+        [DataMember] public double FrequencyHz { get; set; }
+        [DataMember] public double R_ohm { get; set; }
+        [DataMember] public double X_ohm { get; set; }
+        [DataMember] public double T_degC { get; set; }
+        [DataMember] public double Range_ohm { get; set; }
+        [DataMember] public DateTime TimestampLocal { get; set; }
+    }
+
+}
