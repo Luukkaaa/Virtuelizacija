6e01fe5ffd2aed5cfb16bce5eacb96ddf73651ee
Luka <luka.laketiic@gmail.com>
2026-05-14 00:13:10 +0200

    Dodati testovi i namestena putanja

--- Battery/Client/Program.cs
+++ Battery/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Client
             IEisService proxy = factory.CreateChannel();
 
             // Podesite putanju do vašeg testnog dataset foldera
-            string datasetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            string datasetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Dataset");
 
             if (!Directory.Exists(datasetPath))
             {
